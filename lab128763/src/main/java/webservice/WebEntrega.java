df4327849b5cbad18188fb72276ecd9889c26309
Ronaldo Zuchi <faculdadetads@gmail.com>
2016-09-25 12:29:46 -0300

    #Implementados os métodos nos Servlets WebEntrega e WebVenda.

--- lab128763/src/main/java/webservice/WebEntrega.java
+++ lab128763/src/main/java/webservice/WebEntrega.java
@@ -1,5 +1,34 @@
 package webservice;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import ejb.ProcessaEntrega;
+import modelo.Entrega;
+
+@WebService
 public class WebEntrega {
+
+	@EJB
+	ProcessaEntrega processarEntrega;
+
+	@WebMethod(operationName="operacaoDoGetEntrega")
+	@WebResult(name="resultadoEntrega")
+	protected void doGet(@WebParam(name="parReq")HttpServletRequest req, @WebParam(name="parResp") HttpServletResponse resp) throws IOException{
+		System.out.println("Iniciando");
+		Entrega entrega = new Entrega();
+		entrega.setEndreco("Rua das Couves");
+		System.out.println("Iniciando a Entrega");
+		processarEntrega.processandoEntrega(entrega);
+		resp.setContentType("text/html");
+		PrintWriter saida = resp.getWriter();
+		saida.write("<p>Entrega Realizada</p>");
+	}
 
 }
--- lab128763/src/main/java/webservice/WebVenda.java
+++ lab128763/src/main/java/webservice/WebVenda.java
@@ -1,5 +1,36 @@
 package webservice;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import ejb.ProcessaVenda;
+import modelo.Entrega;
+import modelo.Venda;
+
+@WebService
 public class WebVenda {
+
+	@EJB
+	ProcessaVenda processarVenda;
+
+	@WebMethod(operationName="operacaoDoGetVenda")
+	@WebResult(name="resultadoVenda")
+	protected void doGet(@WebParam(name="parReq")HttpServletRequest req, @WebParam(name="parResp") HttpServletResponse resp) throws IOException{
+		System.out.println("Iniciando");
+		Venda venda = new Venda();
+		venda.setItens("Ovo");
+		venda.setQuantidade(12);
+		System.out.println("Iniciando a Venda");
+		processarVenda.processaVenda(venda);;
+		resp.setContentType("text/html");
+		PrintWriter saida = resp.getWriter();
+		saida.write("<p>Venda Realizada</p>");
+	}
 
 }
