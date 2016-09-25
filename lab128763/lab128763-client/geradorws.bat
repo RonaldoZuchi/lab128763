7447b99be3b90a4de57a305a29c0bb05f42d3045
Ronaldo Zuchi <faculdadetads@gmail.com>
2016-09-25 17:04:44 -0300

    #11 - Criado novo projeto lab128763-client.

---
+++ lab128763/lab128763-client/geradorws.bat
@@ -0,0 +1,3 @@
+wsimport -p webservice.WebEntrega -keep -d artefatosWebService http://localhost:8080/lab128763/WebEntrega?wsdl
+wsimport -p webservice.WebVenda -keep -d artefatosWebService http://localhost:8080/lab128763/WebVenda?wsdl
+pause
